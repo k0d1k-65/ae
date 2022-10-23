0868497c8eba81433447e6204b86e4fa331af134
k_yamada <marusu789631@gmail.com>
2022-10-24 07:18:07 +0900

    武器fetcher（仮）追加

---
+++ src/features/Weapons.ts
@@ -0,0 +1,43 @@
+import { EquipmentType } from "../common/constants/EquipmentType";
+import { WeaponType } from "../common/constants/WeaponType";
+import { Equipment } from "../common/types/Equiqment";
+
+const weapons = (() => {
+  const _weapons: Equipment[] = [
+    {
+      name: "ムジマ・ロッド",
+      type: EquipmentType.Weapon,
+      weaponType: WeaponType.Lod,
+      effects: {
+        atk: 23,
+        matk: 190,
+      }
+    },
+    {
+      name: "復元せし時刻みの剣",
+      type: EquipmentType.Weapon,
+      weaponType: WeaponType.Sward,
+      effects: {
+        atk: 165,
+        matk: 20,
+        hp: 500,
+        mp: 100,
+      }
+    },
+    {
+      name: "黎明の刀",
+      type: EquipmentType.Weapon,
+      weaponType: WeaponType.Katana,
+      effects: {
+        atk: 153,
+        matk: 25,
+      }
+    },
+  ];
+
+  return _weapons;
+})();
+
+export function fetchWeapons() {
+  return weapons;
+}

5cdc248502eb85380aa02aa438fa7c0a47f6402b
k_yamada <marusu789631@gmail.com>
2022-10-30 07:23:50 +0900

    ナビゲーションバー追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import UnitBase from './components/UnitBase';
 import Test from './components/test';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { AppBar, createTheme, CssBaseline, IconButton, ThemeProvider, Toolbar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const darkTheme = createTheme({
   palette: {
@@ -13,8 +14,24 @@ const darkTheme = createTheme({
 
 function App() {
   return (
+    // navバー
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
+
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
       <UnitBase />
       {/* <Test /> */}
     </ThemeProvider>

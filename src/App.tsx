f17dcba87a52073ea73d7a7324bf68e405e32302
k_yamada <marusu789631@gmail.com>
2023-03-10 07:10:14 +0900

    App.tsx修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UnitBase from './components/unit/UnitBase';
-import Test from './components/test';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { ThemeColorModeContext } from './common/contexts/ThemeColorModeContext';
 import { NavigationBar } from './components/NavigationBar';
-import EffectBase from './components/unit-effect/EffectBase';
+import UnitBase from './components/unit/UnitBase';
+// import EffectBase from './components/unit-effect/EffectBase';
+// import Test from './components/test';
 
 function App() {
   const [themeColor, setThemeColor] = React.useState<'light' | 'dark'>('dark');
@@ -36,7 +36,7 @@ function App() {
 
         <UnitBase />
 
-        <EffectBase/>
+        {/* <EffectBase/> */}
         {/* <Test /> */}
       </ThemeProvider>
   );

b4536a145796b910f8d78e6815c609916e91890c
k_yamada <marusu789631@gmail.com>
2023-06-21 06:20:29 +0900

    天冥ボーナス入力時のプリセットを設定

--- src/unit-stat/LightShadowBonusForm.tsx
+++ src/unit-stat/LightShadowBonusForm.tsx
@@ -48,6 +48,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_5!}
             title={`${lightShadow}5`}
             setter={wrapSetStatBonus("ls_5")}
+            preset={{
+              hp: 100,
+              mp: 20,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 5,
+              endure: 5,
+              luck: 5,
+              intelligence: 5,
+              split: 5,
+              speed: 5,
+            }}
           />
 
           {/* 天冥 15 */}
@@ -56,6 +70,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_15!}
             title={`${lightShadow}15`}
             setter={wrapSetStatBonus("ls_15")}
+            preset={{
+              hp: 100,
+              mp: 40,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 10,
+              endure: 10,
+              luck: 10,
+              intelligence: 10,
+              split: 10,
+              speed: 10,
+            }}
           />
 
           {/* 天冥 30 */}
@@ -64,6 +92,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_30!}
             title={`${lightShadow}30`}
             setter={wrapSetStatBonus("ls_30")}
+            preset={{
+              hp: 200,
+              mp: 40,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 10,
+              endure: 10,
+              luck: 10,
+              intelligence: 10,
+              split: 10,
+              speed: 10,
+            }}
           />
 
           {/* 天冥 50 */}
@@ -72,6 +114,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_50!}
             title={`${lightShadow}50`}
             setter={wrapSetStatBonus("ls_50")}
+            preset={{
+              hp: 200,
+              mp: 40,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 15,
+              endure: 15,
+              luck: 15,
+              intelligence: 15,
+              split: 15,
+              speed: 15,
+            }}
           />
 
           {/* 天冥 75 */}
@@ -80,6 +136,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_75!}
             title={`${lightShadow}75`}
             setter={wrapSetStatBonus("ls_75")}
+            preset={{
+              hp: 300,
+              mp: 60,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 15,
+              endure: 15,
+              luck: 15,
+              intelligence: 15,
+              split: 15,
+              speed: 15,
+            }}
           />
 
           {/* 天冥 105 */}
@@ -88,6 +158,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_105!}
             title={`${lightShadow}105`}
             setter={wrapSetStatBonus("ls_105")}
+            preset={{
+              hp: 300,
+              mp: 60,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 15,
+              endure: 15,
+              luck: 15,
+              intelligence: 15,
+              split: 15,
+              speed: 15,
+            }}
           />
 
           {/* 天冥 140 */}
@@ -96,6 +180,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_140!}
             title={`${lightShadow}140`}
             setter={wrapSetStatBonus("ls_140")}
+            preset={{
+              hp: 400,
+              mp: 60,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 20,
+              endure: 20,
+              luck: 20,
+              intelligence: 20,
+              split: 20,
+              speed: 20,
+            }}
           />
 
           {/* 天冥 175 */}
@@ -104,6 +202,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_175!}
             title={`${lightShadow}175`}
             setter={wrapSetStatBonus("ls_175")}
+            preset={{
+              hp: 400,
+              mp: 80,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 25,
+              endure: 25,
+              luck: 25,
+              intelligence: 25,
+              split: 25,
+              speed: 25,
+            }}
           />
 
           {/* 天冥 215 */}
@@ -112,6 +224,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_215!}
             title={`${lightShadow}215`}
             setter={wrapSetStatBonus("ls_215")}
+            preset={{
+              hp: 400,
+              mp: 80,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 25,
+              endure: 25,
+              luck: 25,
+              intelligence: 25,
+              split: 25,
+              speed: 25,
+            }}
           />
 
           {/* 天冥 255 */}
@@ -120,6 +246,20 @@ const LightShadowBonusForm = (props: {
             edit={props.unitStat.ls_255!}
             title={`${lightShadow}255`}
             setter={wrapSetStatBonus("ls_255")}
+            preset={{
+              hp: 500,
+              mp: 100,
+              atk: 0,
+              def: 0,
+              matk: 0,
+              mdef: 0,
+              power: 30,
+              endure: 30,
+              luck: 30,
+              intelligence: 30,
+              split: 30,
+              speed: 30,
+            }}
           />
         </DialogContent>
       </Dialog>
--- src/unit-stat/StatBonusEditor.tsx
+++ src/unit-stat/StatBonusEditor.tsx
@@ -2,12 +2,14 @@ import { TextField, MenuItem, ListItemText, Select } from "@mui/material";
 import { EditedOutline } from "../common/EditOutLinedText";
 import { IStatBonus } from "../common/models/UnitModel";
 import { UnitStatBonus, UnitStatBonusType } from "../common/constants/UnitStatType";
+import { UnitStat } from "../units/types/common/UnitStat";
 
 const UnitStatBonusEditorComponent = (props: {
   default: IStatBonus;
   edit: IStatBonus;
   title: string;
   setter: (key: keyof IStatBonus, value: IStatBonus[keyof IStatBonus]) => void;
+  preset?: UnitStat;
 }) => {
   return (
     // スタイルコンプリートボーナス
@@ -19,7 +21,49 @@ const UnitStatBonusEditorComponent = (props: {
       <EditedOutline isEdited={props.default.statType !== props.edit.statType} style={{ width: "45%" }}>
         <Select
           value={props.edit.statType}
-          onChange={(ev) => props.setter("statType", (ev.target.value as UnitStatBonusType) || null)}
+          onChange={(ev) => {
+            const statType = ev.target.value as UnitStatBonusType;
+
+            props.setter("statType", statType || null);
+
+            if (!!statType) {
+              // 補正値の設定がなければ、プリセットを適用
+              if (!props.edit.statAmount) {
+                switch (statType) {
+                  case "HP":
+                    props.setter("statAmount", props.preset?.hp || null);
+                    break;
+                  case "MP":
+                    props.setter("statAmount", props.preset?.mp || null);
+                    break;
+                  case "POWER":
+                    props.setter("statAmount", props.preset?.power || null);
+                    break;
+                  case "ENDURE":
+                    props.setter("statAmount", props.preset?.endure || null);
+                    break;
+                  case "LUCK":
+                    props.setter("statAmount", props.preset?.luck || null);
+                    break;
+                  case "INTELLIGENCE":
+                    props.setter("statAmount", props.preset?.intelligence || null);
+                    break;
+                  case "SPLIT":
+                    props.setter("statAmount", props.preset?.split || null);
+                    break;
+                  case "SPEED":
+                    props.setter("statAmount", props.preset?.speed || null);
+                    break;
+                  default:
+                    break;
+                }
+              }
+              // 演算子の設定がなければ、「.0」にする
+              if (!props.edit.operator) {
+                props.setter("operator", "+");
+              }
+            }
+          }}
           sx={{ width: "100%", height: "56px" }}
         >
           <MenuItem value="">
